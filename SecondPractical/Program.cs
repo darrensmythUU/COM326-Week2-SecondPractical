5aac564b65d29d1e6c1ff1b06564a2aff14a262d
Kiphea <djsmyth05@gmail.com>
2025-10-02 18:44:08 +0100

    Task 1 Complete + Setup Done

--- SecondPractical/Program.cs
+++ SecondPractical/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        PrintMenu();
+    }
+
+    private static void PrintMenu()
+    {
+        // Prints a menu which allows the user to see the below options
+        Console.WriteLine("Please enter a valid option from below: ");
+        Console.WriteLine("1. Hello in French?");
+        Console.WriteLine("2. Hello in Spanish?");
+        Console.WriteLine("3. Hello in German?");
+        Console.WriteLine("4. Hello in Italian?");
+        Console.WriteLine("0. Exit Application.");
+    }
+}
