e6bb5b01f444594fc93e3185d12e0e72c56a5786
Kiphea <djsmyth05@gmail.com>
2025-10-05 23:33:10 +0100

    Task 2 Revision (Try-Catch)

--- SecondPractical/Program.cs
+++ SecondPractical/Program.cs
@@ -5,6 +5,7 @@
         int option = 0;
         PrintMenu();
         option = InputOption(option);
+        GetMessage(option);
     }
 
     private static void PrintMenu()
@@ -20,8 +21,44 @@
 
     private static int InputOption(int methodOption)
     {
-        // Stores the users choice as an integer variable and returns the integer value
-        methodOption = Convert.ToInt32(Console.ReadLine());
+        try
+        {
+            // Stores the users choice as an integer variable and returns the integer value
+            methodOption = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (FormatException ex)
+        {
+            // Handle the case where the input is not valid
+            Console.WriteLine("Please enter a valid number.");
+        }
         return methodOption;
+    }
+
+    private static void GetMessage(int methodOption)
+    {
+        // Determines which language is required and
+        // then returns the correct "Hello" phrase
+        switch (methodOption)
+        {
+            case 0:
+                Console.WriteLine("Goodbye");
+                break;
+            case 1:
+                Console.WriteLine("Bonjour");
+                break;
+            case 2:
+                Console.WriteLine("Ola");
+                break;
+            case 3:
+                Console.WriteLine("Hallo");
+                break;
+            case 4:
+                Console.WriteLine("Ciao");
+                break;
+            default:
+                Console.WriteLine("Please enter a valid option");
+                break;
+
+        }
     }
 }
